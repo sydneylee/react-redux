a7019068922520517bdf05d3bdb239cf35795f89
sookjung.lee <Sookjung.Lee@superchoice.com>
2018-09-08 18:23:43 +1000

    test presentational-counter-component

--- src/store/modules/counter.test.js
+++ src/store/modules/counter.test.js
@@ -24,7 +24,7 @@ describe('counter module:actionCreators and reducer', ()=>{
     });
     //TODO
     //counter라는 reducer함수에 state, action를 인자로 넣어서 reduce()함수를 실행시키는데
-    // 이때, state가 undefined일때는 initialState가 default value로 assign된 상태에서
+    // 이때, state가 undefined(not null) 일때는 initialState가 default value로 assign된 상태에서
     //1) 처음 실행될 때는, action도 없어서 default 에 적용되어 initialState가 적용된 state가 returned되는 상황을 테스트
     //2) counter이라는 reducer함수내에서 INCREMENT와 DECREMENT 두가지 action.type에 대한 처리도 잘 되는지를 테스트
     describe('Reducer', ()=>{
