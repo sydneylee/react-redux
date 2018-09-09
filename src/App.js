250e48c5140f5c9766aa164678853ef89d6eeab1
sookjung.lee <Sookjung.Lee@superchoice.com>
2018-09-09 21:18:28 +1000

    replace Link with NavLink for Active style or hover style

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
 import PostContainer from './containers/post/PostContainer';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+//import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';//TODO : NavLink for active or hover...Style
 import NamesContainer from './containers/names/NamesContainer';
 
 //TODO : // BrowserRouter(=Router) 와 Route 바로 아래에 있는 child 는 single이어야 한다
 // => 여러개일때는  <div></div>로 wrap할 것
 class App extends Component {
   render() {
+      const activeStyle = {
+          color: 'green',
+          fontSize: '2rem'
+      };
     return (
       <div className="App">
         <header className="App-header">
@@ -24,9 +29,9 @@ class App extends Component {
         <BrowserRouter>
             <div>
                 <ul>
-                    <Link to={`/counter`}>COUNTER</Link><span> | </span>
-                    <Link to={`/names`}>NAMES</Link><span> | </span>
-                    <Link to={`/post`}>POST</Link><span> | </span>
+                    <NavLink activeStyle={activeStyle} to={`/counter`}>COUNTER</NavLink><span> | </span>
+                    <NavLink activeStyle={activeStyle} to={`/names`}>NAMES</NavLink><span> | </span>
+                    <NavLink activeStyle={activeStyle} to={`/post`}>POST</NavLink><span> | </span>
                 </ul>
                 <Switch>
                     <Route exact path="/counter">
