34c039f88c02b2e08c8ee72666b558be665c7717
whatifif <whatifif@yahoo.com>
2018-09-13 02:19:24 +1000

    remove yup

--- src/components/namesYUP/NameYUPForm.js
+++ src/components/namesYUP/NameYUPForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import * as yup from 'yup';
+// import PropTypes from 'prop-types';
+// import * as yup from 'yup';
 import * as validator from 'validator';
 
 class NameYUPForm extends Component {
