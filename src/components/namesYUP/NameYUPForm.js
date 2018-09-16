53a8eb1298977cce579f56d6ca7b96524b29c49e
whatifif <whatifif@yahoo.com>
2018-09-16 14:19:09 +1000

    yup.ref('firstname')-good

--- src/components/namesYUP/NameYUPForm.js
+++ src/components/namesYUP/NameYUPForm.js
@@ -321,6 +321,13 @@ import React, {Component} from 'react';
 import * as validator from 'validator';
 import * as yup from 'yup';
 
+yup.addMethod(yup.mixed, 'sameAs', function(ref, message) {
+    return this.test('sameAs', message, function (value) {
+      let other = this.resolve(ref);
+  
+      return !other || !value || value === other;
+    })
+  })
 
 class NameForm extends Component {
     static defaultProps = {};
@@ -338,8 +345,8 @@ class NameForm extends Component {
         disabled: true,
     };
 
-    schemaBlur = yup.object({
-        fullname: yup.object({
+    schemaAsync = yup.object().shape({
+        fullname: yup.object().shape({
             firstname: yup.string().required('first is required').min(3).max(5).test('isDuplicate', 'aaa is duplicate', (value)=>{
                 return new Promise((resolve, reject)=>{
                     setTimeout(()=>{
@@ -347,7 +354,27 @@ class NameForm extends Component {
                     }, 3000)
                 })
             }),
-            lastname : yup.string().required('last  is required').min(3).max(5),
+            // lastname : yup.string().required('last  is required').min(3).max(5).test('isSame', 'lastname should be same as firstname', (value)=>{
+            //     console.log(this.resolve(yup.ref('fullname.firstname')));
+            //     return (value !== this.resolve(yup.ref('fullname.firstname')));
+            // }),
+            // lastname : yup.string().required('last  is required').min(3).max(5).oneOf([yup.ref('fullname.firstname'), null], 'name should be same'),
+            // lastname : yup.string().required('last  is required').min(3).max(5).test('isSame', 'should be same as first', (value)=>{
+            //     return new Promise((resolve, reject)=>{
+            //         // const other = yup.ref('fullname.firstname');
+            //         const other = this.state.fullname.firstname;
+            //         console.log(other);
+            //         resolve(value === other);
+            //     })
+            // }),
+            // lastname : yup.string().required('last  is required').min(3).max(5).test('isSame', 'should be same as first', function(value){
+            //     return new Promise((resolve, reject)=>{
+            //         resolve(value===yup.resolve(yup.ref('fullname.firstname')))
+            //     })
+            // }),
+            // lastname : yup.string().sameAs(yup.ref('fullname.firstname'), 'passwords do not match')//notworking
+            // lastname : yup.string().required('last  is required').min(3).max(5).oneOf([this.newState.fullname.firstname], 'should be same' )
+            lastname : yup.string().oneOf([yup.ref('firstname')], 'should be same as firstname' )
         })
     });
 
@@ -358,7 +385,10 @@ class NameForm extends Component {
         })
     });
 
-    newState;
+    newState = {
+        ...this.state
+    }
+
     doValidate = (e)=>{
         const targetName = e.target.name;
         const targetValue = e.target.value;
@@ -377,11 +407,20 @@ class NameForm extends Component {
         this.setState(this.newState);
 
         const path = 'fullname.'+targetName;
-        // const schema = e.type === 'blur'? this.schemaBlur: this.schema;
-        const schema = e.type === 'blur'? this.schema: this.schemaBlur;
-        
+        // const schema = e.type === 'blur'? this.schemaAsync: this.schema;
+        // const schema = e.type === 'blur'? this.schema: this.schemaAsync;
+        const schema = this.schemaAsync;
+ 
         schema.validateAt(path, this.newState).then(value=>{
+        // schema.validate(this.newState).then(value=>{
             delete this.newState.errors[targetName];
+            if(targetName=== 'firstname'){
+                delete this.newState.errors['firstname']; 
+                delete this.newState.errors['lastname']; 
+            }else if(targetName=== 'lastname'){
+                delete this.newState.errors['lastname']; 
+                delete this.newState.errors['firstname']; 
+            }
             this.newState.loading = false;
             this.newState.disabled = Object.keys(this.newState.errors).length > 0 || Object.keys(this.newState.touched).length < 2;
             this.setState(this.newState)
