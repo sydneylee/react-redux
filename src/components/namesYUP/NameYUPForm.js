8b149b51621b20e139967c4915c890708c87fc88
whatifif <whatifif@yahoo.com>
2018-09-17 18:04:18 +1000

    yup.equalTo is added-good

--- src/components/namesYUP/NameYUPForm.js
+++ src/components/namesYUP/NameYUPForm.js
@@ -321,13 +321,13 @@ import React, {Component} from 'react';
 import * as validator from 'validator';
 import * as yup from 'yup';
 
-yup.addMethod(yup.mixed, 'sameAs', function(ref, message) {
-    return this.test('sameAs', message, function (value) {
+yup.addMethod(yup.mixed, 'equalTo', function(ref, message) {
+    const msg = message || '${path} should match';
+    return this.test('equalTo', msg, function (value) {
       let other = this.resolve(ref);
-  
       return !other || !value || value === other;
     })
-  })
+})
 
 class NameForm extends Component {
     static defaultProps = {};
@@ -347,13 +347,15 @@ class NameForm extends Component {
 
     schemaAsync = yup.object().shape({
         fullname: yup.object().shape({
-            firstname: yup.string().required('first is required').min(3).max(5).test('isDuplicate', 'aaa is duplicate', (value)=>{
-                return new Promise((resolve, reject)=>{
-                    setTimeout(()=>{
-                        resolve(value!=='aaa');
-                    }, 3000)
-                })
-            }),
+            // firstname: yup.string().required('first is required').min(3).max(5).test('isDuplicate', 'aaa is duplicate', (value)=>{
+            //     return new Promise((resolve, reject)=>{
+            //         setTimeout(()=>{
+            //             resolve(value!=='aaa');
+            //         }, 3000)
+            //     })
+            // }),
+            firstname : yup.string().required().min(3).max(5).equalTo(yup.ref('lastname'), 'should be same as lastname'),
+
             // lastname : yup.string().required('last  is required').min(3).max(5).test('isSame', 'lastname should be same as firstname', (value)=>{
             //     console.log(this.resolve(yup.ref('fullname.firstname')));
             //     return (value !== this.resolve(yup.ref('fullname.firstname')));
@@ -372,9 +374,10 @@ class NameForm extends Component {
             //         resolve(value===yup.resolve(yup.ref('fullname.firstname')))
             //     })
             // }),
-            // lastname : yup.string().sameAs(yup.ref('fullname.firstname'), 'passwords do not match')//notworking
+            // lastname : yup.string().equalTo(yup.ref('fullname.firstname'), 'passwords do not match')//notworking
             // lastname : yup.string().required('last  is required').min(3).max(5).oneOf([this.newState.fullname.firstname], 'should be same' )
-            lastname : yup.string().oneOf([yup.ref('firstname')], 'should be same as firstname' )
+            // lastname : yup.string().oneOf([yup.ref('firstname')], 'should be same as firstname' )
+            lastname : yup.string().required().min(3).max(5).equalTo(yup.ref('firstname'))
         })
     });
 
@@ -415,11 +418,11 @@ class NameForm extends Component {
         // schema.validate(this.newState).then(value=>{
             delete this.newState.errors[targetName];
             if(targetName=== 'firstname'){
-                delete this.newState.errors['firstname']; 
+                // delete this.newState.errors['firstname']; 
                 delete this.newState.errors['lastname']; 
             }else if(targetName=== 'lastname'){
-                delete this.newState.errors['lastname']; 
                 delete this.newState.errors['firstname']; 
+                // delete this.newState.errors['lastname']; 
             }
             this.newState.loading = false;
             this.newState.disabled = Object.keys(this.newState.errors).length > 0 || Object.keys(this.newState.touched).length < 2;
